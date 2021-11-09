461a0fcb0bf5ff07b015c4abadeb6f7090fa1823
munzii <munzii422@gmail.com>
2021-11-09 10:54:57 +0900

    study

---
+++ src/P1008.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Double a = sc.nextDouble();
+		Double b = sc.nextDouble();
+		sc.close();
+		System.out.println(a/b);
+
+	}
+
+}
